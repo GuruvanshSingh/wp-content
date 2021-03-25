104f62e0d0c6f02a31a73a2776439c758a99bac0
Guruvansh Singh <guruvanshsingh@dcmail.ca>
2021-03-25 19:34:23 -0400

    Foundations re-Uploaded

--- themes/blushthm/functions.php
+++ themes/blushthm/functions.php
@@ -133,9 +133,9 @@ function blushthm_scripts() {
 
 		//Foundations Scripts
 	//css
-	wp_enqueue_style( 'foundation-style', get_template_directory_uri(). './assets/css/vendor/foundation.css');
-	wp_enqueue_script( 'foundation-script', get_template_directory_uri(). './assets/js/vendor/foundation.js');
-
+	wp_enqueue_style( 'foundation-style', get_template_directory_uri(). '/assets/css/vendor/foundation.css');
+	//js
+	wp_enqueue_script( 'foundation-script', get_template_directory_uri(). '/assets/js/vendor/foundation.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
