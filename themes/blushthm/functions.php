f0890b9792650d2dc11873f9fe09b5c0ebceb0d6
Guruvansh Singh <guruvanshsingh@dcmail.ca>
2021-03-25 19:27:34 -0400

    commit

--- themes/blushthm/functions.php
+++ themes/blushthm/functions.php
@@ -133,7 +133,8 @@ function blushthm_scripts() {
 
 		//Foundations Scripts
 	//css
-	wp_enqueue_style('foundation-style', get_template_directory_uri(),'/assets/css/vendor/foundation.css');
+	wp_enqueue_style( 'foundation-style', get_template_directory_uri(), '/assets/css/vendor/foundation.css');
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
