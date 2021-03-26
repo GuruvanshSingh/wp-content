7beb1567a3a714ec2c3094aeeafab800bd127506
Guruvansh Singh <guruvanshsingh@dcmail.ca>
2021-03-26 09:16:53 -0400

    comments added

--- themes/blushthm/sidebar.php
+++ themes/blushthm/sidebar.php
@@ -7,11 +7,15 @@
  * @package Blush_Theme
  */
 
+/**
+ * check if sidebar allowed
+ */
 if ( ! is_active_sidebar( 'sidebar' ) ) {
 	return;
 }
 ?>
 
+<!--  Widget section -->
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar' ); ?>
 </aside><!-- #secondary -->
